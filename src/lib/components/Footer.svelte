838f5d6a20db813b8aeb789f57b5f0527aff9d60
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-03-14 09:22:30 +0100

    feat: privacy politcy link added

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
 <Footer>
 	<FooterCopyright by="Jean-Baptiste™" year={2024} />
 	<FooterLinkGroup ulClass="flex flex-row justify-center items-center mt-3 text-sm text-gray-500 dark:text-gray-400 mb-5">
-		<FooterLink href="/">Politique de confidentialité</FooterLink>
+		<FooterLink href="https://www.privacypolicies.com/live/b12de624-c789-492d-8f9b-6bad2c8409aa">Politique de confidentialité</FooterLink>
 		<FooterLink href="/#Contact">Contact</FooterLink>
 	</FooterLinkGroup>
 </Footer>
