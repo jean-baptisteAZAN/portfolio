4214c0bee155a97c7eb21a1c08a6cbf43fac6559
Jean-Baptiste Azan <azan.jean-baptiste@epitech.eu>
2026-02-10 17:48:23 +0000

    feat: cv

---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<title>CV - Jean-Baptiste Azan</title>
+</svelte:head>
+
+<div class="cv-container">
+	<object data="/cv-azan-jean-baptiste.pdf" type="application/pdf" width="100%" height="100%">
+		<p>
+			Impossible d'afficher le PDF.
+			<a href="/cv-azan-jean-baptiste.pdf" download>Télécharger le CV</a>
+		</p>
+	</object>
+</div>
+
+<style>
+	.cv-container {
+		width: 100%;
+		height: 100vh;
+	}
+
+	object {
+		border: none;
+	}
+</style>
